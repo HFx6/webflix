7c6cf29d877abb30edf6b8c2fb71df5e6c312f6d
hfx6 <haydenfx6@gmail.com>
2024-03-07 21:10:09 +1300

    adding placeholder curation data for testing

--- src/app/components/medialistwrapper.jsx
+++ src/app/components/medialistwrapper.jsx
@@ -15,6 +15,70 @@ async function getGenre() {
 	const res = await fetch(process.env.URL + "/api/discover");
 	return res.json();
 }
+
+const curatedLists = [
+	{
+		title: "Trending",
+		endpoint: "/trending/all/week",
+		apiParams: {
+			language: "en-US",
+		},
+	},
+	{
+		title: "Popular movies on Netflix",
+		endpoint: "/discover/movie",
+		apiParams: {
+			language: "en-US",
+			primary_release_year: 2024,
+			sort_by: "popularity.desc",
+			include_adult: true,
+			include_video: false,
+			with_watch_providers: 8,
+			with_original_language: "en",
+			watch_region: "US",
+		},
+	},
+	{
+		title: "Popular shows on Netflix",
+		endpoint: "/discover/tv",
+		apiParams: {
+			language: "en-US",
+			first_air_date: "2024-01-01",
+			include_adult: true,
+			include_null_first_air_dates: false,
+			sort_by: "popularity.desc",
+			watch_region: "US",
+			with_original_language: "en",
+			with_watch_providers: 8,
+		},
+	},
+	{
+		title: "We Think You'll Love These",
+		endpoint: "",
+		apiParams: {},
+	},
+	{
+		title: "Suspenseful Movies",
+		endpoint: "",
+		apiParams: {},
+	},
+	{
+		title: "K-Dramas",
+		endpoint: "",
+		apiParams: {},
+	},
+	{
+		title: "US TV Shows",
+		endpoint: "",
+		apiParams: {},
+	},
+	{
+		title: "Documentaries",
+		endpoint: "",
+		apiParams: {},
+	},
+];
+
 export default async function MediaListWrapper() {
 	const movieData = getData();
 	const genreData = getGenre();
