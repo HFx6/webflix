96abc17e6a64ff9d1b11a5dfa9120329993dc06c
hfx6 <haydenfx6@gmail.com>
2024-03-25 01:00:08 +1300

    incorrect db path

---
+++ src/app/(root)/newprofile/page.jsx
@@ -0,0 +1,56 @@
+"use client";
+
+import Link from "next/link";
+
+import Image from "next/image";
+
+import { db, login } from "../../../utils/db";
+
+export default function Page() {
+	const loginWithUser = async (username) => {
+		await login(username);
+	};
+	return (
+		<div className="flex items-center m-auto justify-center h-[100vh] flex-col gap-7">
+			<p className="font-normal text-5xl">Who&apos;s Watching?</p>
+			<div className="flex gap-7 items-center">
+				<div className="profileblock  flex flex-col items-center gap-4">
+					<div>
+						<Link href="/browse">
+							<Image
+								src="/bouba.png"
+								width={150}
+								height={150}
+								alt="empty"
+								onClick={() => loginWithUser("Bouba")}
+							/>
+						</Link>
+					</div>
+					<p className="text-[#6D6D6D]">Bouba</p>
+				</div>
+				<div className="profileblock flex flex-col items-center gap-4">
+					<div>
+						<Link href="/browse">
+							<Image
+								src="/kiki.png"
+								width={150}
+								height={150}
+								alt="empty"
+								onClick={() => loginWithUser("Kiki")}
+							/>
+						</Link>
+					</div>
+					<p className="text-[#6D6D6D]">Kiki</p>
+				</div>
+				<div className="flex flex-col items-center gap-4">
+					<div className="text-6xl h-[150px] w-[150px] flex">
+						<div className="bg-[#7f7f7f] rounded-full m-auto text-[#181818] w-[110px] aspect-square flex items-center justify-center text-6xl">
+							<ImPlus />
+						</div>
+					</div>
+					<p className="text-[#6D6D6D]">Add Profile</p>
+				</div>
+			</div>
+		</div>
+	);
+}
