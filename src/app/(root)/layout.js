36efdd571791d91b67384443f5243869e821577b
hfx6 <haydenfx6@gmail.com>
2024-03-31 20:26:35 +1300

    using netflix sans font now instead of inter

--- src/app/(root)/layout.js
+++ src/app/(root)/layout.js
@@ -1,10 +1,45 @@
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
 import NavBar from "./components/navbar";
 import Footer from "./components/footer";
 
-const inter = Inter({ subsets: ["latin"] });
+
+const netflixSans = localFont({
+	src: [
+	  {
+		path: "../../../public/fonts/NetflixSans_W_Th.woff",
+		weight: "100",
+		style: "normal",
+	  },
+	  {
+		path: "../../../public/fonts/NetflixSans_W_Lt.woff",
+		weight: "300",
+		style: "normal",
+	  },
+	  {
+		path: "../../../public/fonts/NetflixSans_W_Rg.woff",
+		weight: "400",
+		style: "normal",
+	  },
+	  {
+		path: "../../../public/fonts/NetflixSans_W_Md.woff",
+		weight: "700",
+		style: "normal",
+	  },
+	  {
+		path: "../../../public/fonts/NetflixSans_W_Bd.woff",
+		weight: "800",
+		style: "normal",
+	  },
+	  {
+		path: "../../../public/fonts/NetflixSans_W_Blk.woff",
+		weight: "900",
+		style: "normal",
+	  },
+	],
+	variable: '--font-netflix-sans',
+  });
 
 export const metadata = {
 	title: "webflix",
@@ -17,7 +52,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>
+			<body className={`${netflixSans.variable} font-netflixsans`}>
 				<div className="relative flex min-h-screen flex-col bg-background">
 					<NavBar />
 					<main className="flex-1">{children}</main>
