852a0e2856a13a704f2b2e7a141a49454cee7deb
hfx6 <haydenfx6@gmail.com>
2024-04-02 17:03:43 +1100

    added layout

---
+++ src/app/(watch)/layout.js
@@ -0,0 +1,59 @@
+import localFont from "next/font/local";
+import "./globals.css";
+
+import NavBar from "./components/navbar";
+import Footer from "./components/footer";
+
+const netflixSans = localFont({
+  src: [
+    {
+      path: "../../../public/fonts/NetflixSans_W_Th.woff",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../../../public/fonts/NetflixSans_W_Lt.woff",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../../public/fonts/NetflixSans_W_Rg.woff",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../../public/fonts/NetflixSans_W_Md.woff",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../../public/fonts/NetflixSans_W_Bd.woff",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../../../public/fonts/NetflixSans_W_Blk.woff",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+  variable: "--font-netflix-sans",
+});
+
+export const metadata = {
+  title: "webflix",
+  description: "One month build of Netflix",
+  icons: {
+    icon: "/logo/logo.png",
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${netflixSans.variable} font-netflixsans`}>
+        <main className="flex-1">{children}</main>
+      </body>
+    </html>
+  );
+}
